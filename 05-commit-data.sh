9ce9b3e283b5df705ae4a8c86466277de30ef542
pli01 <pli01@github.com>
2020-03-14 18:33:21 +0100

    Deploy via Makefile

---
+++ 05-commit-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git config user.email "pli01@github.com"
+git config user.name "pli01"
+
+git checkout data
+
+git status -s
+git add .
+git commit -m "Deploy via Makefile"
+git push -f -q https://${GITHUB_TOKEN}@github.com/pli01/extract-data data
