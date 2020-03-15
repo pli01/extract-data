3ac6b1be5748baaceda821c4d60ca24c228d06bf
pli01 <pli01@github.com>
2020-03-15 18:55:24 +0100

    csv + json

--- 03-extract-from-text.sh
+++ 03-extract-from-text.sh
@@ -14,4 +14,10 @@ for txt in $dst/*.txt; do
  value=$(grep Cela $txt | sed -e 's/.*Cela porte//g; s/le nombre de cas.*//g' | awk '{ print $2 }')
  echo "$date $value"
 done | tee $data/data.txt
+
+echo "# convert to $data/data.csv"
+awk '{ print "\""$1"\""","$2 }' < data/data.txt > data/data.csv
+
+echo "# convert to $data/data.json"
+cat $data/data.txt |awk ' { printf("[\"%s\",%d]\n",$1,$2) } ' | sed '$!s/$/,/' |awk 'BEGIN{print "[ [\"index\",0],"} { print } END { print "]" } '  > $data/data.json 
 exit 0
--- 04-graph-data.sh
+++ 04-graph-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-echo "# generate data/data.png"
+echo "# generate data/data.png from data/data.txt"
 docker run --rm -v $(pwd):/work remuslazar/gnuplot -c graph.gnuplot
 exit 0
